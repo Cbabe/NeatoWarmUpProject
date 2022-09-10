0a270b7860d8d7c249dab8a092ffc394a5c6a432
Cbabe <cbabe@olin.edu>
2022-09-10 14:57:46 -0400

    Teleop

--- warmup_project/warmup_project/teleop.py
+++ warmup_project/warmup_project/teleop.py
@@ -1,6 +1,56 @@
-def main():
-    print('Hi from warmup_project.')
+"""Read bump data to tell neato to stop"""
 
+import rclpy
+from rclpy.node import Node
+from neato2_interfaces.msg import Bump
+from geometry_msgs.msg import Twist
+import tty
+import select
+import sys
+import termios
+
+class teleop(Node):
+    def __init__(self):
+        super().__init__('receive_bump_data')
+        self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
+        print("WASD key board: W forward, S Backward, A Turn Right, D Turn Left, Any key to stop.")
+        self.go_forwards()
+    
+    def go_forwards(self):
+        key=None
+        while key != '\x03':
+            #Get key value - waits until user presses key
+            key = getKey()
+            speed_msg = Twist()
+            if key=='w':
+                #Forward
+                speed_msg.linear.x = 1.0
+            elif key=='s':
+                #Backward
+                speed_msg.linear.x = -1.0
+            elif key=='a':
+                #Turn Right
+                speed_msg.angular.z = 1.0
+            elif key=='d':
+                #Turn Left
+                speed_msg.angular.z = -1.0
+            #Publish
+            self.publisher.publish(speed_msg)
+
+
+def main(args=None):
+    rclpy.init(args=args)         # Initialize communication with ROS
+    node = teleop()   # Create our Node
+    rclpy.spin(node)              # Run the Node until ready to shutdown
+    rclpy.shutdown()              # cleanup
+
+def getKey():
+    settings = termios.tcgetattr(sys.stdin)
+    tty.setraw(sys.stdin.fileno())
+    select.select([sys.stdin], [], [], 0)
+    key = sys.stdin.read(1)
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
 
 if __name__ == '__main__':
     main()
